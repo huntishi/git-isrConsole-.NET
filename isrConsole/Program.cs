e5c7ca3a22fe9ecea2a35417c9bacfc0469aecf5
KHANI <israqkhan@gmail.com>
2017-04-09 13:33:50 -0700

    This is base code

---
+++ isrConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isrConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.Write("First application");
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+    }
+}
